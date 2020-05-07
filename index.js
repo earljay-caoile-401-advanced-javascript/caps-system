9718a3bb85b0fa7f2a38d6e64142a3d8f83dd231
Earl Jay Caoile <unfie555@gmail.com>
2020-05-06 22:47:01 -0700

    checkpoint: almost having it look like the example. How do I add pickup before the object?

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const globalEmitter = require('./lib/events.js');
 
 const logPayload = (payload) => {
-  console.log(payload);
+  console.log('EVENT', payload, '\n');
 };
 
 globalEmitter.on('pickup', logPayload);
+globalEmitter.on('in-transit', logPayload);
+globalEmitter.on('confirmation', logPayload);
 
 require('./lib/vendor.js');
+require('./lib/driver.js');
